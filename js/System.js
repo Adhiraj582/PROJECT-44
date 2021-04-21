6a1e0fe77e1b8312ebed75815a2e658bda9320ff
Adhiraj <77384552+Adhiraj582@users.noreply.github.com>
2021-04-21 21:05:29 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,15 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(actualCode,enteredCode0){
+        if(actualCode === enteredCode0)
+            return true
+        else 
+            return false    
+          
+        }
+    
+
+}
